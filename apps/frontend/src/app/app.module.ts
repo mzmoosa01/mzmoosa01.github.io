7f7aecd42e050a3db8bf44655cf416ac477ece08
Zubair Moosa <mzmoosa01@gmail.com>
2021-04-03 15:50:41 +0200

    Feat: Initial hero component

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { TechnologyTemplateComponent } from './technology-template/technology-template.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent],
+  declarations: [AppComponent, HeroComponent, TechnologyTemplateComponent],
   imports: [BrowserModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
