6e2d64b9580e7ac3a6190501af56c6f5142c4046
Zubair Moosa <mzmoosa01@gmail.com>
2021-04-03 11:09:44 +0200

    Initial setup + MDB installation

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeroComponent],
+  imports: [BrowserModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
