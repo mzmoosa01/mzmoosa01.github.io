60ce9b9d9306155f0f0d0551b95aa9f2fe834451
Zubair Moosa <mzmoosa01@gmail.com>
2021-04-17 12:30:23 +0200

    Feat: Typing animation and scroll

--- apps/frontend/src/app/hero/hero.component.ts
+++ apps/frontend/src/app/hero/hero.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { TechnologyTemplateComponent } from '../technology-template/technology-template.component';
 
 @Component({
   selector: 'hero-component',
@@ -6,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent implements OnInit {
-  constructor() {}
+  i = 0;
+  //@ViewChild(TechnologyTemplateComponent) technologiesComp;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.typing();
+  }
+
+  typing = () => {
+    const txt =
+      'I design, code and deploy reliable scalable and maintainable software solutions'; /* The text */
+    const speed = 50; /* The speed/duration of the effect in milliseconds */
+
+    // for (let j = 0; j < txt.length; j++) {
+    //   setTimeout(() => this.typeLetter(txt.charAt(j), 'demo'), speed);
+    // }
+
+    if (this.i < txt.length) {
+      document.getElementById('demo').innerHTML += txt.charAt(this.i);
+      this.i++;
+      setTimeout(this.typing, speed);
+    }
+  };
+
+  typeLetter(letter: string, elementId: string) {
+    document.getElementById(elementId).innerHTML += letter;
+  }
+
+  public scrollToElement(elementId: string) {
+    const el = document.getElementById(elementId);
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
 }
